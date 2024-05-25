115e713aa404294b1c5a0e70e602a343137b0901
MiguelMarquezPosso <marquez_miguela@javeriana.edu.co>
2024-05-25 12:48:20 -0500

    Clase constantes creada.

---
+++ src/main/java/org/example/integracion/Constantes.java
@@ -0,0 +1,7 @@
+package org.example.integracion;
+
+public class Constantes {
+    public static final String USERNAME = "is357815";
+    public static final String PASSWORD = "Rufito14";
+    public static final String THINCONN = "jdbc:oracle:thin:@orion.javeriana.edu.co:1521/LAB";
+}
